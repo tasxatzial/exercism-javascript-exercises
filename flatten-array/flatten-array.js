38d0a8a885f8ec559df3bc95e1285e061d941359
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-03-07 00:06:16 +0200

    flatten: minor refactoring

--- flatten-array/flatten-array.js
+++ flatten-array/flatten-array.js
@@ -4,22 +4,20 @@
 //
 
 export const flatten = (arr, depth = Infinity) => {
-    return _flatten(arr, [], 0);
-    
-    function _flatten(arr, flattened, currDepth) {
-        if (currDepth > depth) {
-            flattened.push(arr);
-            return;
-        }
-        for (let el of arr) {
-            if (Array.isArray(el)) {
-                _flatten(el, flattened, currDepth + 1)
-            } else {
-                if (el !== null) {
-                  flattened.push(el);
-                }
-            }
-        }
-        return flattened;
+  return _flatten(arr, [], 0);
+  
+  function _flatten(arr, flattened, currDepth) {
+    if (currDepth > depth) {
+      flattened.push(arr);
+      return;
     }
+    for (const el of arr) {
+      if (Array.isArray(el)) {
+        _flatten(el, flattened, currDepth + 1)
+      } else if (el !== null) {
+        flattened.push(el);
+      }
+    }
+    return flattened;
+  }
 };
