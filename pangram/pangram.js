5b2de8d76cc2885a12283a152d355a6c135a6c04
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-03-08 21:03:55 +0200

    pangram: refactoring

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,9 @@
+/* Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan gramma, "every letter")
+is a sentence using every letter of the alphabet at least once. */
+
 const ALPHABET = [...'abcdefghijklmnopqrstuvwxyz'];
 
-/* Returns true iff string is pangram, that is it contains all letters of the
-alphabet at least once (case insensitive) */
 export const isPangram = (string) => {
   let lowerCaseString = string.toLowerCase();
-  return ALPHABET.every(letter => RegExp(letter).test(lowerCaseString));
+  return ALPHABET.every(letter => lowerCaseString.includes(letter));
 };
