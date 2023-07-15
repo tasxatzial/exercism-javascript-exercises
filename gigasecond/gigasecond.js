64df85fc34a8e4a01dd65f97e5fb180fccbd540f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-07-15 19:29:27 +0300

    gigasecond: refactor

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,14 +1,9 @@
 const GIGASECOND = 1e9; // seconds
-
-/* Converts seconds to milliseconds */
 const secondsToMilliseconds = (seconds) => 1e3 * seconds;
 
-/* Returns the date that is 1 gigasecond (10^9s) after the provided startDate */
+/* Returns the date when 1 gigasecond (10^9s) is added to startDate */
 export const gigasecond = (startDate) => {
-  let startTime = startDate.getTime();
-
-  /* convert gigasecond to millisecons since startTime is in milliseconds */
+  let startTime = startDate.getTime(); // ms
   let endTime = startTime + secondsToMilliseconds(GIGASECOND);
-
   return new Date(endTime);
 };
